ef052c6d59fee84c1dad3ee9c7140b95623b841e
LeonidK <Leokom96@gmail.com>
2023-10-04 21:38:20 +0200

    updage api

--- src/API/Service/Src/Controllers/WordsController.cs
+++ src/API/Service/Src/Controllers/WordsController.cs
@@ -4,8 +4,6 @@ using Service.Models;
 using Service.Src.Models;
 using State.Src;
 using State.Src.Commands;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Service.Src.Controllers
@@ -25,27 +23,6 @@ namespace Service.Src.Controllers
         {
             var result = await _mediator.Send(new GetWordsCommand(from, to, type));
             return new WordsViewModel(result, from, to, type);
-        }
-
-        [HttpGet("random")]
-        public async Task<IActionResult> GetRandomAsync(Language from, Language to, WordsType type)
-        {
-            var result = await _mediator.Send(new GetWordsCommand(from, to, type));
-            return Ok(result.ElementAt(new Random().Next(result.Count)));
-        }
-
-        [HttpPost("check")]
-        public async Task<IActionResult> CheckAsync([FromBody] CheckWordRequestModel request)
-        {
-            var result = await _mediator.Send(new CheckWordCommand()
-            {
-                To = request.To,
-                FromValue = request.FromValue,
-                ToValue = request.ToValue,
-                Type = request.Type,
-                from = request.from
-            });
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
 
     }
