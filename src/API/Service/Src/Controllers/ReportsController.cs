3e973215c641f1aacdfdced190870a20013aa859
LeonidK <Leokom96@gmail.com>
2023-10-06 16:45:19 +0200

    reports c

---
+++ src/API/Service/Src/Controllers/ReportsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using State.Src.Commands;
+using System.Threading.Tasks;
+
+namespace Service.Src.Controllers
+{
+    [ApiController, Route("api/reports")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReportsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(ulong id)
+        {
+            return Ok(1);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] CreateReportRequestModel request)
+        {
+            return Ok(1);
+        }
+
+    }
+}
---
+++ src/API/Service/Src/Models/CreateReportRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace State.Src.Commands
+{
+    public class CreateReportRequestModel
+    {
+        public ulong UserId { get; set; }
+
+        public ulong TotalCount { get; set; }
+
+        public ulong ErrorsCount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
